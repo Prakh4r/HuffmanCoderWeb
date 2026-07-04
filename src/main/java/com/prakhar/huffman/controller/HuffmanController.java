4324dda0e6f39a84644331f9ef51a61902d85bb2
Prakh4r <prakhardhawan1218@gmail.com>
2026-07-05 01:53:54 +0530

    Implement Huffman file compression REST endpoint

--- src/main/java/com/prakhar/huffman/controller/HuffmanController.java
+++ src/main/java/com/prakhar/huffman/controller/HuffmanController.java
@@ -4,6 +4,17 @@ import com.prakhar.huffman.service.HuffmanService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+
 @RestController
 public class HuffmanController {
     private final HuffmanService huffmanService;
@@ -15,5 +26,40 @@ public class HuffmanController {
     @GetMapping("/hello")
     public String home(){
         return huffmanService.getWelcomeMessage();
+    }
+    //ResponseEntity - construction of an HTTP response.
+    /*  | HTTP Method | Meaning                   |
+        | ----------- | ------------------------- |
+        | GET         | Give me something         |
+        | POST        | I'm sending you something |
+        | PUT         | Replace something         |
+        | DELETE      | Remove something          |
+    */
+    //@RequestParam means Find the uploaded part named file and put it into this variable.
+
+    @PostMapping("/compress")
+    public ResponseEntity<byte[]> compress(
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+
+        byte[] compressedData = huffmanService.compress(file);
+
+        //Building of a header file so that our browser can understand what to do with those bytes
+
+        /*ResponseEntity.ok() -> returns status code*/
+
+        /*Content-Type -> since binary data, we use MediaType.APPLICATION_OCTET_STREAM
+            It literally tells the browser: "This response contains arbitrary binary data."
+        */
+
+        /*
+            Content-Disposition -> tells chrome what to do with those bytes
+            Example - attachment; filename="compressed.huff"
+            so chrome will download the file with those bytes
+        */
+
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"compressed.huff\"")
+                .body(compressedData);
     }
 }
