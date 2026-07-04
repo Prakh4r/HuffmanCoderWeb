e22e2e11e3d37ae4e8ab0d1a46bc02dd1e2c2ba2
Prakh4r <prakhardhawan1218@gmail.com>
2026-07-05 00:42:59 +0530

    Implement Huffman compression service with temporary file handling

--- src/main/java/com/prakhar/huffman/service/HuffmanService.java
+++ src/main/java/com/prakhar/huffman/service/HuffmanService.java
@@ -4,6 +4,7 @@ import com.prakhar.huffman.compression.HuffmanCompressor;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +18,30 @@ public class HuffmanService {
         Path inputFile = Files.createTempFile("input-", ".txt");
         Path outputFile = Files.createTempFile("output-", ".huff");
 
-        return null;
+        try{
+            //converts or writes MultipartFile into inputFile so that our engine can understand
+            file.transferTo(inputFile);
+
+
+            //since compressor is a normal java Class spring does not create the object of it in its own
+            HuffmanCompressor compressor = new HuffmanCompressor();
+            compressor.compress(inputFile.toString(), outputFile.toString());
+
+
+            /*  output.huff (Temp File)
+                        │
+                        ▼
+                Files.readAllBytes(...)
+                        │
+                        ▼
+                    byte[]
+            */
+            //HTTP responses don't send Java File objects. They send bytes over the network.
+
+            return Files.readAllBytes(outputFile);
+        }finally {
+            Files.deleteIfExists(inputFile);
+            Files.deleteIfExists(outputFile);
+        }
     }
 }
